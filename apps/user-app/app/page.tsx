93eea48c311e9f09fc4e3164ebf223db7be013c2
naveenReddy <naveenreddy7252@gmail.com>
2025-03-30 07:54:24 +0530

    addinf=g random space

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,6 +6,8 @@ import {redirect} from 'next/navigation'
 import { authOptions } from '../lib/auth';
 
 
+
+
 export default async function Home() {
   const session =await getServerSession(authOptions);
  if(session?.user){
