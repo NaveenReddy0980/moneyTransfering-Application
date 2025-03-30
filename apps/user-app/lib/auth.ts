772c6f00881ee0a4c316a8d81e6387972e2f9524
naveenReddy <naveenreddy7252@gmail.com>
2025-03-30 13:10:15 +0530

    changed call back url of next auth

--- apps/user-app/lib/auth.ts
+++ apps/user-app/lib/auth.ts
@@ -57,7 +57,7 @@ export const authOptions = {
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
         async redirect() {
-            return "/dashboard"
+            return "http://16.171.165.136:3005//dashboard"
         },
         // TODO: can u fix the type here? Using any is bad
         async session({ token, session }: any) {
